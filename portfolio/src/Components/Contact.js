be96bc38ee3fe9c1dd5881ca56478248ee974648
Capril1042 <Caprilewis1042@gmail.com>
2018-08-20 15:48:56 -0400

    Contact icons

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
+let iconStyle = {
+    fontSize :'200px'
+};
 
 class Contact extends Component {
   
     render(){
         return(
            <div>
-           contact me
+           <div className="what">
+           A few ways to get in touch...
+           </div>
+           <i className="fa fa-github" style={iconStyle}></i>
+           <i className="fa fa-linkedin-square" style ={iconStyle}></i>
+           <i className="fa fa-envelope" style ={iconStyle}></i>
            </div>
         );
     }
