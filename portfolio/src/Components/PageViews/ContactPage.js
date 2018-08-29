3e89db1ca35e719ea0c767931544cddeeff690f2
Capril1042 <Caprilewis1042@gmail.com>
2018-08-29 17:24:16 -0400

    added contact form

--- portfolio/src/Components/PageViews/ContactPage.js
+++ portfolio/src/Components/PageViews/ContactPage.js
@@ -29,6 +29,17 @@ class ContactPage extends Component {
         return(
             <section id ="contact" className="contact">
            <a  class="btn btn-large btn primary" href="mailto:caprilewis1042@gmail.com"><i className="fa fa-envelope" style={iconStyle}></i></a>
+ <form className="form" name="contact" method="POST" netlify>
+  <p>
+    <label>Email: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
            <ColoredLine/>
            <a class="btn btn-large btn primary" href="https://www.linkedin.com/in/capri-lewis-64a73410b/"><i className="fa fa-linkedin-square" style ={iconStyle}></i> </a>
            <a class="btn btn-large btn primary" href="https://github.com/Capril1042"><i className="fa fa-github" style ={iconStyle}></i></a>
