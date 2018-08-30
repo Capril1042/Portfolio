a69a49252211e826c5875c639426fd485e249e13
Capril1042 <Caprilewis1042@gmail.com>
2018-08-30 17:33:37 -0400

    contact form somewhat responsive

--- portfolio/src/Components/PageViews/ContactPage.js
+++ portfolio/src/Components/PageViews/ContactPage.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
 import '../main.css'
 
 let iconStyle = {
     color:'#A9754F',
-    fontSize :'200px',
-    padding: '10px',
+    fontSize :'150px',
+    padding: '20px',
     margin: '3%',
     
 };
@@ -28,22 +29,38 @@ class ContactPage extends Component {
     render(){
         return(
             <section id ="contact" className="contact">
-           <a  class="btn btn-large btn primary" href="mailto:caprilewis1042@gmail.com"><i className="fa fa-envelope" style={iconStyle}></i></a>
- <form className="form" name="contact" method="POST" netlify>
-  <p>
-    <label>Email: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+                      <div className="container">
+                    <Form  className="form" name="contact" method="POST" netlify>
+                        <h2 className="header"> Contact Me! </h2>
+                            <FormGroup row className="mb-2 mr-sm-2 mb-sm-0">
+                                <Label >Name: </Label> 
+                                    <Input type="text" name="name" />
+                            </FormGroup>
+
+                            <FormGroup  row className="mb-2 mr-sm-2 mb-sm-0">
+                                <Label>Email: </Label> 
+                                    <Input type="text" name="name" />
+                            </FormGroup>
+
+                            <FormGroup row className="mb-2 mr-sm-2 mb-sm-0">
+                                <Label >Message: </Label>
+                                    <Input type="textarea" name="message"/>
+                            </FormGroup >
+
+                            <FormGroup>
+                                <Button className="sendbutton" type="submit">Send</Button>
+                            </FormGroup>
+                    </Form>
+                    
+                   </div>
+                   <div className="line">
            <ColoredLine/>
+           </div>
+           <div className="contactbuttons">
+           <a  class="btn btn-large btn primary" href="mailto:caprilewis1042@gmail.com"><i className="fa fa-envelope" style={iconStyle}></i></a>
            <a class="btn btn-large btn primary" href="https://www.linkedin.com/in/capri-lewis-64a73410b/"><i className="fa fa-linkedin-square" style ={iconStyle}></i> </a>
            <a class="btn btn-large btn primary" href="https://github.com/Capril1042"><i className="fa fa-github" style ={iconStyle}></i></a>
-           
+           </div>
             </section>
         );
     }
