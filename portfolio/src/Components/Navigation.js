6137dfe66eb288cf9bf456e5895e11634280c0a4
Capril1042 <Caprilewis1042@gmail.com>
2019-06-10 11:23:44 -0400

    updates navigation

--- portfolio/src/Components/Navigation.js
+++ portfolio/src/Components/Navigation.js
@@ -8,7 +8,6 @@ class Navigation extends Component {
  <a href="#home">Home</a>
   <a href="#about">About Me</a>
   <a href="#projects">Projects</a>
-  {/* <a href="#blog">Blog</a> */}
   <a href="#resume">Resume</a>
   <a href="#contact">Contact</a>
 </div>
